7931130c2f181ee30b6a0893d2a12d0b37a22cc5
Ujjawal <ujjawal.sharma@preplaced.in>
2024-02-09 15:29:35 +0530

    feat: added zustand

--- src/component/main/list/list.tsx
+++ src/component/main/list/list.tsx
@@ -1,4 +1,5 @@
 import SearchIcon from "../../../assets/main/search.svg";
+import { NewChatButton } from "./new-chat-button";
 export const List = () => {
   return (
     <div className="h-full w-full border py-[30px] px-[30px] relative">
@@ -13,6 +14,9 @@ export const List = () => {
           className="absolute top-1/2 transform -translate-y-1/2 left-2 w-5 h-5 text-gray-500"
           aria-hidden="true"
         />
+      </div>
+      <div className="absolute right-2 bottom-2 border rounded-xl bg-[#dbdcff] aspect-square h-[50px]">
+        <NewChatButton />
       </div>
     </div>
   );

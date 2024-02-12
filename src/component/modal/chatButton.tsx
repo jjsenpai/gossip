210173c5682bc3190ff52ee2e60f010bf63d2adc
Sumit <135352466+sumitcoder01@users.noreply.github.com>
2024-02-12 22:37:43 +0530

    Update chatButton.tsx

--- src/component/modal/chatButton.tsx
+++ src/component/modal/chatButton.tsx
@@ -1,6 +1,6 @@
 export const NewChatButton = ({showModal}) => {
   return (
-      <button onClick={showModal} className=" bg-green-500 abo px-5 py-2 text-center rounded-md absolute right-2 bottom-1">
+      <button onClick={showModal} className=" bg-green-500 px-5 py-2 text-center rounded-md absolute right-2 bottom-1">
         chat
       </button>
   );

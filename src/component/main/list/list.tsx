d4df7f0ba7275e56630f6575bb33fd429d88cbea
simran693 <simrantomar693@gmail.com>
2024-02-17 12:52:16 +0530

    skeleton

--- src/component/main/list/list.tsx
+++ src/component/main/list/list.tsx
@@ -7,7 +7,7 @@ const ChatSkeleton: React.FC = () => {
     for (let i = 0; i < 10; i++) {
       messages.push(
         <div key={i} className="flex items-start space-x-4">
-          <div className="bg-gray-300 h-12 w-16 rounded-full animate-pulse"></div>
+          <div className="bg-gray-300 h-12 w-20 rounded-full animate-pulse"></div>
           <div className="bg-gray-300 h-12 rounded animate-pulse w-full"></div>
         </div>
       );
